f4dae98baef9e7ed7d2c5a3f1fe9bbcc70dbed7d
LZhiYue <q2912982@hotmail.com>
2016-12-20 17:22:23 +0800

    给数据结构加入了日期变量

--- app/src/main/java/datastruct/DiaryMessage.java
+++ app/src/main/java/datastruct/DiaryMessage.java
@@ -1,5 +1,7 @@
 package datastruct;
 
+import java.util.Date;
+
 /**
  * 用于表示评论日记的消息的数据结构
  * 其变量成员与数据库的表中一致
@@ -11,12 +13,14 @@ public class DiaryMessage {
     public String messageOwner; // 评论的用户的ID
     public String messageContent; // 评论的内容
     public String objectId;   // 该评论在数据库中的ID
+    public Date date;  // 日期
 
-    public DiaryMessage(String author, String diaryId, String messageOwner, String messageContent, String objectId) {
+    public DiaryMessage(String author, String diaryId, String messageOwner, String messageContent, String objectId, Date date) {
         this.author = author;
         this.diaryId = diaryId;
         this.messageOwner = messageOwner;
         this.messageContent = messageContent;
         this.objectId = objectId;
+        this.date = date;
     }
 }
