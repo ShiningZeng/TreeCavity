3c8522de38a36c7eaee94df3b628350a549a94d5
Embers <475974010@qq.com>
2016-12-21 19:30:59 +0800

    增加了消息推送

--- app/src/main/java/com/example/sk2014/treecavity/NavigationPage.java
+++ app/src/main/java/com/example/sk2014/treecavity/NavigationPage.java
@@ -1,19 +1,35 @@
 package com.example.sk2014.treecavity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import pushservice.MyService;
+
 public class NavigationPage extends AppCompatActivity {
+    private MyService ms;
+    private ServiceConnection sc = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            ms = ((MyService.MyBinder) iBinder).getService();
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_page);
-
+        ms = new MyService(NavigationPage.this);
+        ms.request();
         LinearLayout edit_button = (LinearLayout) findViewById(R.id.edit_button);
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
