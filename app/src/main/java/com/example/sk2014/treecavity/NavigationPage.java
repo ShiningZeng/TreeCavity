28a48880299ad22158e8cade06fa872698ab355e
Embers <475974010@qq.com>
2016-12-21 18:42:55 +0800

    增加了在线推送消息和回复消息推送

--- app/src/main/java/com/example/sk2014/treecavity/NavigationPage.java
+++ app/src/main/java/com/example/sk2014/treecavity/NavigationPage.java
@@ -1,18 +1,42 @@
 package com.example.sk2014.treecavity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVInstallation;
+import com.avos.avoscloud.PushService;
+import com.avos.avoscloud.SaveCallback;
+
+import pushservice.MyService;
+
 public class NavigationPage extends AppCompatActivity {
+    private MyService ms;
+    private Context context;
+    private ServiceConnection sc = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            ms = ((MyService.MyBinder) iBinder).getService();
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = NavigationPage.this;
         setContentView(R.layout.activity_navigation_page);
-
+        ms = new MyService(NavigationPage.this);
+        ms.request();
         Button edit_button = (Button)findViewById(R.id.edit_button);
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +68,12 @@ public class NavigationPage extends AppCompatActivity {
                 startActivity(new Intent(NavigationPage.this, OtherDiaryDetail.class));
             }
         });
+        AVInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                String installationId = AVInstallation.getCurrentInstallation().getInstallationId();
+            }
+        });
+        PushService.setDefaultPushCallback(this, MyDiaryDetail.class);
     }
 }
