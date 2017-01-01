2cca2e6012eeb11401886a8f93031d2fc33e9c2b
廖竞翔 <Liaojx3@mail2.sysu.edu.cn>
2017-01-01 22:33:27 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/sk2014/treecavity/MainActivity.java
+++ app/src/main/java/com/example/sk2014/treecavity/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
